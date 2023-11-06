a9e25643b62775831cbdcbf8e9108a1e981c3fd4
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-06 22:53:44 +0100

    changes to content

--- IdeaProjects/My First Project with Java/Topics/Formatted output/Simple combination/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {        
-        String name = "galactic hunter";
-        String learning  = "java";
-        System.out.printf("%s learning %s", name, learning);
-
-    }
-}
