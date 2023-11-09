3b667a6104fe70f7d4dabd5aa09e694018c6a018
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-09 14:46:41 +0100

    cat added

--- IdeaProjects/JavaPractice/src/Constructors/Animals.java
+++ IdeaProjects/JavaPractice/src/Constructors/Animals.java
@@ -3,5 +3,9 @@ package Practice.Hyperskill.Methods.JavaPractice.src.Constructors;
 public class Animals {
     String name;
     int age;
-    String breed;
+    //String breed;
+    public Animals(String name, int age){
+     this.name = name;
+     this.age = age;
+    }
 }
---
+++ IdeaProjects/JavaPractice/src/Constructors/Kittens.java
@@ -0,0 +1,9 @@
+package Practice.Hyperskill.Methods.JavaPractice.src.Constructors;
+
+public class Kittens {
+    public static void main(String[] args) {
+        Animals myCat = new Animals("Jerry", 3);
+        System.out.println(myCat.name);
+        System.out.println(myCat.age);
+    }
+}
