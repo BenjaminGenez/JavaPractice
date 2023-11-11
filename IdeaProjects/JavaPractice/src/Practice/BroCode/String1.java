7c96d820c355fea83acb99cba64ef416a73baee7
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-11 22:21:45 +0100

    string method

---
+++ IdeaProjects/JavaPractice/src/Practice/BroCode/String1.java
@@ -0,0 +1,11 @@
+package Practice.Hyperskill.Methods.JavaPractice.src.Practice.BroCode;
+
+public class String1 {
+    public static void main(String[] args) {
+
+        String name = "people";
+        boolean result = name.equals("people");
+        System.out.println(result);
+
+    }
+}
