537a61ea2099aaae99f4189027458debf6e203dd
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-11 22:22:22 +0100

    string method second test

--- IdeaProjects/JavaPractice/src/Practice/BroCode/String1.java
+++ IdeaProjects/JavaPractice/src/Practice/BroCode/String1.java
@@ -4,7 +4,7 @@ public class String1 {
     public static void main(String[] args) {
 
         String name = "people";
-        boolean result = name.equals("people");
+        boolean result = name.equals("cats");
         System.out.println(result);
 
     }
