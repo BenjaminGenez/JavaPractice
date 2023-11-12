8192977eb149ea10b294d6457f103be020bfb065
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-12 18:15:40 +0100

    scanner test

---
+++ IdeaProjects/JavaPractice/src/Constructors/Chat.java
@@ -0,0 +1,15 @@
+package Practice.Hyperskill.Methods.JavaPractice.src.Constructors;
+
+import java.util.Scanner;
+
+public class Chat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your name");
+        String user = scanner.next();
+
+
+
+        System.out.println(user);
+    }
+}
