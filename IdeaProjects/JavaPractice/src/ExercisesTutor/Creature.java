8a44cadf5e54b505ab5098e3d8242539ba9ceae1
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-12 20:44:06 +0100

    exercise to make added

---
+++ IdeaProjects/JavaPractice/src/ExercisesTutor/Creature.java
@@ -0,0 +1,47 @@
+package Practice.Hyperskill.Methods.JavaPractice.src.ExercisesTutor;
+
+
+/**Exercise: Creature Hierarchy
+ Background:
+ Imagine a world with different types of creatures. Each creature has its own characteristics and behaviors.
+
+ Task:
+ Create a Base Class:
+
+ Name it Creature.
+ Define common attributes like name (String) and age (int).
+ Include a constructor to initialize these attributes.
+ Add getters and setters for each attribute (encapsulation in action!).
+ Inheritance:
+
+ Create two subclasses of Creature named Bird and Mammal.
+ For Bird, add an attribute wingSpan (double).
+ For Mammal, add an attribute furColor (String).
+ Each subclass should have its own constructor to initialize its specific attributes along with the inherited ones.
+ Implement getters and setters for new attributes.
+ Unique Behaviors:
+
+ In Bird, add a method fly() that prints something like "Flapping wings and flying!".
+ In Mammal, add a method run() that prints something like "Running on the ground!".
+ Control Flow in Main Method:
+
+ Create a main method in another class.
+ Instantiate a Bird and a Mammal.
+ Use control flow (if-else) to check the type of each created object.
+ If it's a Bird, call the fly() method.
+ If it's a Mammal, call the run() method.
+ Bonus:
+
+ Add a method in Creature called displayInfo() which prints out the creature's details.
+ Override this method in both Bird and Mammal to include their specific attributes.
+ Objectives:
+ Practice using constructors for object initialization.
+ Understand inheritance and how subclass constructors relate to base class constructors.
+ Apply encapsulation with private attributes and public getters/setters.
+ Implement control flow to execute methods based on object type.
+ Bonus: Get a grip on method overriding.
+ This exercise will give you a well-rounded practice session on these concepts. And remember,
+ if your creatures start behaving oddly, it's probably just a bug in the matrix, nothing a little debugging can't fix!
+ 😉 Happy coding!*/
+public class Creature {
+}
