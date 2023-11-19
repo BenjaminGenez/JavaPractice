436b2e68bf18eccb5d87ac4c6ed44bf9b76a46d0
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-19 20:38:16 +0100

    zoo 39

---
+++ IdeaProjects/JavaPractice/src/Practice/Arrays/Array10.java
@@ -0,0 +1,8 @@
+package Practice.Hyperskill.Methods.JavaPractice.src.Practice.Arrays;
+
+public class Array10 {
+    public static void main(String[] args) {
+        int people[];    //declaring array
+        people = new int[20];  // allocating memory to array
+    }
+}
