966cf422fb1a5bce5a345abf06d991486dc3de90
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-19 20:44:30 +0100

    zoo 40

--- IdeaProjects/JavaPractice/src/Practice/Arrays/Array10.java
+++ IdeaProjects/JavaPractice/src/Practice/Arrays/Array10.java
@@ -1,8 +1,15 @@
 package Practice.Hyperskill.Methods.JavaPractice.src.Practice.Arrays;
 
+import java.util.Scanner;
+
 public class Array10 {
     public static void main(String[] args) {
-        int people[];    //declaring array
-        people = new int[20];  // allocating memory to array
+        int people[];
+        people = new int[20];
+
+        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+
+
     }
 }
