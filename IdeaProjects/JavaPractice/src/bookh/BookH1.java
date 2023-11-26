72db86ce06ac49db06ee8543ffb9d674a6290d1b
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-26 22:26:17 +0100

    book2

--- IdeaProjects/JavaPractice/src/bookh/BookH1.java
+++ IdeaProjects/JavaPractice/src/bookh/BookH1.java
@@ -5,4 +5,10 @@ public class BookH1 {
     String name = "Dirk"; x = x * 17;
     System.out.print("x is " + x);
     double d = Math.random();
+    while (x > 12)
+    { x = x - 1; }
+    for (int i = 0; i < 10; i = i + 1)
+    {
+        System.out.print("i is now " + i);
+    }
 }
