6d342cb49b232909c91ddc69a98df4ca91ca117a
BenjaminGenez <benjaminyahari25@gmail.com>
2023-11-26 22:25:03 +0100

    book1

---
+++ IdeaProjects/JavaPractice/src/bookh/BookH1.java
@@ -0,0 +1,8 @@
+package Practice.Hyperskill.Methods.JavaPractice.src.bookh;
+
+public class BookH1 {
+    int x = 3;
+    String name = "Dirk"; x = x * 17;
+    System.out.print("x is " + x);
+    double d = Math.random();
+}
