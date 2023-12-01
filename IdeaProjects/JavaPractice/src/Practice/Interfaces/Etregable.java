0297585708d63f1b5e692bce9be1ced2a8373183
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-01 22:50:26 +0100

    homework7

--- IdeaProjects/JavaPractice/src/Practice/Interfaces/Etregable.java
+++
@@ -1,9 +0,0 @@
-package Practice.Hyperskill.Methods.JavaPractice.src.Practice.Interfaces;
-
-public interface Entregable {
-    void entregar();
-    void devolver();
-    boolean isEntregado();
-
-    //int compareTo(Object a);
-}
