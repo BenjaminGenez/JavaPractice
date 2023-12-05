b1506ffc2add2a17ff58e75383855a87c970b1c0
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-05 23:16:47 +0100

    sjf6

--- IdeaProjects/JavaPractice/src/Copilot/cop.java
+++ IdeaProjects/JavaPractice/src/Copilot/cop.java
@@ -8,6 +8,7 @@ package Practice.Hyperskill.Methods.JavaPractice.src.Copilot;
 public class cop {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println();
     }
 
 }
