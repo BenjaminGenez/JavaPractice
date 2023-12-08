d7ba7951ffd9932bffae517062d21ab96e2692c1
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-08 21:05:23 +0100

    exercism problems2 6

--- IdeaProjects/JavaPractice/src/yBookExamples/TestLog.java
+++ IdeaProjects/JavaPractice/src/yBookExamples/TestLog.java
@@ -4,5 +4,6 @@ public class TestLog {
     public static void main(String[] args) {
 
         System.out.println(LogLevels.message("[ERROR]: Invalid operation"));
+        System.out.println(LogLevels.reformat("[ERROR]: Invalid operation");
     }
 }
