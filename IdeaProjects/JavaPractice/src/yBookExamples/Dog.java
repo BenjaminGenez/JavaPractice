96989d8848ef94cf4515f2932610dc37bef5039a
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-10 22:11:29 +0100

    head first java - objects new 1

--- IdeaProjects/JavaPractice/src/yBookExamples/Dog.java
+++ IdeaProjects/JavaPractice/src/yBookExamples/Dog.java
@@ -1,10 +1,17 @@
 package yBookExamples;
 
 public class Dog {
+
     String name;
     int size;
 
     public void bark(){
         System.out.println("wof");
+    }
+    public void bark(int numOfBarks){
+        while(numOfBarks > 0){
+            System.out.println("wof");
+            numOfBarks = numOfBarks - 1;
+        }
     }
 }
