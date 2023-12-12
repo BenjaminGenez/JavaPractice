6e465fe7966d6966df092dec77f4722f1a64b11f
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-12 23:04:55 +0100

    disco de roer3

---
+++ IdeaProjects/JavaPractice/src/basics/DataTypes/BubleSort.java
@@ -0,0 +1,18 @@
+package basics.DataTypes;
+
+ public class BubleSort {
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j+1] and array[j]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        while ()
+    }
+}
