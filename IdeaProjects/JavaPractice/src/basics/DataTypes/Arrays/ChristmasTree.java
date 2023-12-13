9f2046076df1f11f18f11fabb69aa9ba6ddd60ec
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-13 22:25:00 +0100

    factorial3

---
+++ IdeaProjects/JavaPractice/src/basics/DataTypes/Arrays/ChristmasTree.java
@@ -0,0 +1,19 @@
+package basics.DataTypes.Arrays;
+
+public class ChristmasTree {
+    public static void main(String[] args) {
+        int[] numeros = { 1, 2, 3, 4, 5, 6 };
+        int count = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print(numeros[count] + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
