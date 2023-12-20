784660383eda62a2f06e2066ff80e04bc068c6be
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-20 22:07:03 +0100

    welcome2

---
+++ IdeaProjects/JavaPractice/src/advance/Exeception/Welcome.java
@@ -0,0 +1,10 @@
+package advance.Exeception;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+    while (true) {
+        System.out.println("Welcome to Java");
+    }
+}
