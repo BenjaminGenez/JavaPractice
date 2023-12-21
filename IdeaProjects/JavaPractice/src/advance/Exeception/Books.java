bb99fd5141a9bd1e50d33a5fc5f26bbaba48b9c1
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-21 22:28:55 +0100

    book1

---
+++ IdeaProjects/JavaPractice/src/advance/Exeception/Books.java
@@ -0,0 +1,12 @@
+package advance.Exeception;
+
+public class Books {
+
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        String str = "Hello";
+        System.out.println("Welcome to Java");
+
+    }
+}
