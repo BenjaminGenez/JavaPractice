e87a2d709414cf19054d402908f43428c1e1bf2b
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-21 22:31:07 +0100

    tree3

--- IdeaProjects/JavaPractice/src/advance/Exeception/Tree.java
+++ IdeaProjects/JavaPractice/src/advance/Exeception/Tree.java
@@ -4,8 +4,19 @@ public class Tree {
     public static void main(String[] args) {
         String tree = "Pine";
         int size = 0;
+        boolean flag = true;
+
         if (tree.equals("Pine")) {
             size = 10;
+        }
+        else if (tree.equals("Oak")) {
+            size = 20;
+        }
+        else if (tree.equals("Maple")) {
+            size = 30;
+        }
+        else {
+            flag = false;
         }
     }
 }
