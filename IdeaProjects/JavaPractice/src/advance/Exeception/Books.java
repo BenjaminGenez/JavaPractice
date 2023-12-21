810816b6296c0ff0bda2afdec842aa32b84eb3af
BenjaminGenez <benjaminyahari25@gmail.com>
2023-12-21 22:30:23 +0100

    tree2

--- IdeaProjects/JavaPractice/src/advance/Exeception/Books.java
+++ IdeaProjects/JavaPractice/src/advance/Exeception/Books.java
@@ -9,4 +9,6 @@ public class Books {
         System.out.println("Welcome to Java");
 
     }
+
+
 }
---
+++ IdeaProjects/JavaPractice/src/advance/Exeception/Tree.java
@@ -0,0 +1,11 @@
+package advance.Exeception;
+
+public class Tree {
+    public static void main(String[] args) {
+        String tree = "Pine";
+        int size = 0;
+        if (tree.equals("Pine")) {
+            size = 10;
+        }
+    }
+}
