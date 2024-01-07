32c20fba366133c755514927d94db61f1314bc39
BenjaminGenez <benjaminyahari25@gmail.com>
2024-01-07 20:56:23 +0100

    int 2

--- IdeaProjects/JavaPractice/src/advance/integrals.java
+++ IdeaProjects/JavaPractice/src/advance/integrals.java
@@ -1,4 +1,53 @@
 package advance;
 
 public class integrals {
+    public static void main(String[] args) {
+        String tree = "Pine";
+        int size = 0;
+        boolean flag = true;
+
+        if (tree.equals("Pine")) {
+            size = 10;
+        }
+        else if (tree.equals("Oak")) {
+            size = 20;
+        }
+        else if (tree.equals("Maple")) {
+            size = 30;
+        }
+        else {
+            flag = false;
+        }
+
+        while (flag) {
+            System.out.println("The size of the tree is " + size);
+            flag = false;
+        }
+
+        int year = 2016;
+        flag = true;
+
+        if (year == 2016) {
+            System.out.println("The year is " + year);
+        }
+        else if (year == 2017) {
+            System.out.println("The year is " + year);
+        }
+        else if (year == 2018) {
+            System.out.println("The year is " + year);
+        }
+        else {
+            flag = false;
+        }
+
+        while (flag) {
+            System.out.println("The year is " + year);
+            flag = false;
+        }
+
+        flag = true;
+        String str = "Hello";
+        System.out.println("Welcome to Java");
+
+    }
 }
